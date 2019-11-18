4faa845970f8d4191c26bef730b93f6e42606005
Romain <romain.angier33125@gmail.com>
2019-11-18 21:32:37 +0100

    Commit

--- site-apel/hf/debutpage.php
+++ site-apel/hf/debutpage.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8">
-    <title>POO - SLAM5</title>
+    <title>APEL Champagnat</title>
 
-    <link rel="stylesheet" href="styles/st1.css" type="text/css">
-    <script src="../scripts/ajax.js">
-    </script>
-    <script src="scripts/gestprof.js">
-    </script>
+    <link rel="stylesheet" href="styles/styles.css" type="text/css">
+    <script src="scripts/ajax.js" type="text/javascript"></script>
 </head>
 <body>
 <div id="contenu">
